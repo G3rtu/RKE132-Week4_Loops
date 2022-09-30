3e71911a95193d560dd0660770a30f40c3711a0d
Gertu Piho <gertu.piho@tktk.ee>
2022-09-30 11:13:21 +0300

    Loops

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+for(int i = 0; i <5; i ++)
+{
+    Console.WriteLine("We don't need no education.");
+}
+Console.WriteLine("Have a nice day.");
+//Console.WriteLine("We don't need no education.");
